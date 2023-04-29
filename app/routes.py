3f6a6c888d530adc3388348b3e62f87c6fad6187
Darana <abdullahsaud03@gmail.com>
2023-04-29 15:59:15 +0500

    implemented jinja 2 templates to the home page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Abdullah'}
+    posts = [
+        {
+            'author': {'username': 'Darana'},
+            'body': 'Aloha, fellow mates'
+        },
+        {
+            'author': {'username': 'Granger'},
+            'body': 'Harry Potter reference'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts = posts)
