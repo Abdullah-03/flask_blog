0328adf138878893966fccb83f97c0b2a81709a7
Darana <abdullahsaud03@gmail.com>
2023-05-01 23:44:47 +0500

    user login, required login, used flask-login extension. Full implementation remaining.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'  # Lets flask-login know what the login page is for when it has to redirect users when they
+# access content that is for members only
 
 from app import routes, models
