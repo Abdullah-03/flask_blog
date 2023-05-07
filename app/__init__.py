103adc610a6280f8ed90c315dda316bec3c2fefe
Darana <abdullahsaud03@gmail.com>
2023-05-07 15:53:05 +0500

    created custom templates to show errors (404 and 500 for now) while also implementing an smtp server that email whenever an error occurs

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import os
+import logging
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +17,23 @@ login = LoginManager(app)
 login.login_view = 'login'  # Lets flask-login know what the login page is for when it has to redirect users when they
 # access content that is for members only
 
-from app import routes, models
+
+from app import routes, models, errors
+
+
+#  smtp handler for default flask logger to send out error emails
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Flask Blog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
