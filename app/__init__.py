656ca0a6399256b466974a2acf74928e53f99df0
Darana <abdullahsaud03@gmail.com>
2023-05-14 13:06:21 -0700

    Added error logging within a file, fixed error where users could change usernames to ones that were already in use (wasn't possible as sqlalchemy raised exception but now it is handled by the application itself).

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 app = Flask(__name__)
@@ -37,3 +37,15 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        #  Error logging to file
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/flask_blog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Flask startup')
