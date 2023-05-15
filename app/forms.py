bc5dc2c173e30ab3774609edca20cfa7e800289a
Darana <abdullahsaud03@gmail.com>
2023-05-14 19:01:56 -0700

    Users can now follow/unfollow other users. Added test cases for prior features and for follow as well.

--- app/forms.py
+++ app/forms.py
@@ -44,3 +44,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username).first()
             if user is not None:
                 raise ValidationError('username already in use')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
